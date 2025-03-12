849d9f509b061503294aed82f511d249ec61efa8
Ryan Ankersen <ryan.m.ankersen@gmail.com>
2025-03-12 09:40:37 -0500

    Removed redundant functions in py-scripts

--- Project Folder Template/py-scripts/generate_cdf.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-def generate_cdf(input_array):
-
-    sorted_array = np.sort(input_array)
-    cdf_array = np.array(range(len(sorted_array))) / float(len(sorted_array))
-
-    # Returns a sorted x array and the CDF array
-    return sorted_array, cdf_array
