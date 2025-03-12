452eae591d9b0c9660a68db6d32add72e391c20e
Ryan Ankersen <ryan.m.ankersen@gmail.com>
2025-03-12 10:24:58 -0500

    generate_cdf Null handling

--- Project Folder Template/py-scripts/ankersen_functions.py
+++ Project Folder Template/py-scripts/ankersen_functions.py
@@ -12,9 +12,9 @@ def ks_statistic(x1,x2)
 
 # CDF array generation, returning a sorted input array, and associated CDF function. Useful for Plotting 
 def generate_cdf(input_array):
-    
+
+    input_array = input_array[~np.isnan(input_array)]
     sorted_array = np.sort(input_array)
-    sorted_array = sorted_array[~np.isnan(sorted_array)]
     cdf_array = np.array(range(len(sorted_array))) / float(len(sorted_array))
 
     # Returns a sorted x array and the CDF array
